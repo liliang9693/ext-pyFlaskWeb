83eccfa8fb374fa2946e88efc1ec91d77e9b9282
liliang9693 <liliang_wk@163.com>
2022-07-19 20:16:07 +0800

    0.0.1

---
+++ README.md
@@ -0,0 +1,73 @@
+# Flask网页服务器
+
+
+![](./python/_images/featured.png)
+
+---------------------------------------------------------
+
+## Table of Contents
+
+* [URL](#url)
+* [Summary](#summary)
+* [Blocks](#blocks)
+* [License](#license)
+* [Supported targets](#Supportedtargets)
+
+## URL
+* Project URL : ```https://github.com/liliang9693/ext-pyFlaskWeb```
+
+* Tutorial URL : ```https://mindplus.dfrobot.com.cn/extensions-user```
+
+    
+
+
+
+
+## Summary
+使用Mind+V1.7.2 RC3.0及以上版本包含python模式的版本，在python模式用户库中加载此扩展，可实现使用纯python创建一个网页并在浏览器访问。
+
+使用Flask库创建网页服务器，使用dominate库实现python生成html代码。
+
+
+
+## Blocks
+积木分成两部分，前半部分是搭建网页服务器功能，后半部分是网页内容设计。
+
+![](./python/_images/block1.png)
+
+![](./python/_images/block2.png)
+
+
+
+## Examples
+> 使用逻辑：
+- 1、新建网页对象
+- 2、给网页对象添加内容
+- 3、将网页对象生成html文件
+- 4、启动Flask服务器
+- 5、增加网页路由，设定执行函数
+- 6、执行函数中根据标签执行对应动作
+- 7、浏览器打开网页查看效果
+
+![](./python/_images/example.png)
+![](./python/_images/example2.png)
+
+
+## License
+
+MIT
+
+## Supported targets
+
+MCU                | JavaScript    | Arduino   | MicroPython    | Python 
+------------------ | :----------: | :----------: | :---------: | -----
+arduino        |             |              |             | 
+micro:bit        |             |              |             | 
+esp32        |             |              |             | 
+unihiker        |             |              |             | √
+PC        |             |              |             | √
+
+## Release Logs
+
+* V0.0.1  基础功能完成
+
---
+++ python/main.ts
@@ -0,0 +1,198 @@
+
+
+//% color="#1296db" iconWidth=50 iconHeight=40
+namespace pyFlaskWeb{
+
+
+    //% block="初始化Flask" blockType="command"
+    export function flask_init(parameter: any, block: any) {
+        Generator.addImport(`from flask import Flask,Response,render_template,request`)
+        Generator.addImport(`flask_app = Flask(__name__)`)
+ 
+    }
+
+    //% block="启动Flask服务器 端口[PORT]" blockType="command"
+    //% PORT.shadow="number" PORT.defl="8000"
+    export function flask_run(parameter: any, block: any) {
+        let port=parameter.PORT.code;
+        Generator.addCode(`flask_app.run(host='0.0.0.0', port=${port}, threaded=True)`)
+ 
+    }
+
+
+    //% block="增加一个网页路由 标签[URL] 执行函数[FUNC] 方法[GP]" blockType="command"
+    //% URL.shadow="normal" URL.defl="index"
+    //% FUNC.shadow="normal" FUNC.defl="route_func"
+    //% GP.shadow="dropdown" GP.options="GP"
+    export function add_route(parameter: any, block: any) {
+        let url=parameter.URL.code;
+        let func=parameter.FUNC.code;
+        let gp=parameter.GP.code;
+        Generator.addInit(`add_route_${url}`,`@flask_app.route('/${url}',methods=${gp})
+def route_${url}():
+    return rec_${func}()`)
+    
+    }
+
+    //% block="执行函数[FUNC]" blockType="hat"
+    //% FUNC.shadow="normal" FUNC.defl="route_func"
+    export function add_route_fun(parameter: any, block: any) {
+        let func=parameter.FUNC.code;
+        Generator.addEvent(`add_route_${func}`,`rec_${func}`,``)
+    
+    }
+    //% block="返回数据[FUNC]" blockType="command"
+    //% FUNC.shadow="string" FUNC.defl="route_func"
+    export function add_route_fn(parameter: any, block: any) {
+        let func=parameter.FUNC.code;
+        Generator.addCode(`return ${func}`)
+    
+    }
+
+    //% block="返回网页文件[HTML].html" blockType="command"
+    //% HTML.shadow="normal" HTML.defl="index"
+    export function add_route_html(parameter: any, block: any) {
+        let html=parameter.HTML.code;
+        Generator.addCode(`return render_template("${html}.html")`)
+    
+    }
+
+    //% block="获取网络请求[REQ]" blockType="reporter"
+    //% REQ.shadow="dropdown" REQ.options="REQ"
+    export function get_request(parameter: any, block: any) {
+        let req=parameter.REQ.code;
+        Generator.addCode(`request.${req}`)
+    
+    }
+
+   //% block="---"
+   export function noteSep() {
+
+}
+
+    //% block="新建一个网页对象[DOC] 页面标题[TXT] [ALIGN]" blockType="command"
+    //% DOC.shadow="normal" DOC.defl="doc"
+    //% TXT.shadow="string" TXT.defl="我的第一个网页"
+    //% ALIGN.shadow="dropdown" ALIGN.options="ALIGN"
+    export function html_init(parameter: any, block: any) {
+        let doc=parameter.DOC.code;
+        let title=parameter.TXT.code;
+        let align=parameter.ALIGN.code;
+        align=align.replace(/"/g,"")
+        Generator.addImport(`import dominate\nfrom dominate.tags import *`)
+        Generator.addCode(`${doc} = dominate.document(title=${title})`)
+        Generator.addCode(`${doc}.body['style']="text-align:${align};"`)
+        Generator.addDeclaration("ls","sc=''")
+        Generator.addCode(`with ${doc}.head:
+    script(sc)
+        `)
+    
+    }
+  
+
+    //% block="开始给网页对象[DOC]中添加内容：" blockType="command"
+    //% DOC.shadow="normal" DOC.defl="doc"
+    export function html_doc_with(parameter: any, block: any) {
+        let doc=parameter.DOC.code;
+        Generator.addCode(`with ${doc}:`)
+    
+    }
+    //% block="|- 添加[TITLETYPE] [TXT] [ALIGN][COLOR]" blockType="command"
+    //% TXT.shadow="string" TXT.defl="标题文字"
+    //% TITLETYPE.shadow="dropdown" TITLETYPE.options="TITLETYPE"
+    //% ALIGN.shadow="dropdown" ALIGN.options="ALIGN"
+    //% COLOR.shadow="colorPalette" 
+    export function html_add_title(parameter: any, block: any) {
+        let type=parameter.TITLETYPE.code;
+        let txt=parameter.TXT.code;
+        let align=parameter.ALIGN.code;
+        let color=parameter.COLOR.code;
+        align=align.replace(/"/g,"")
+        color=color.replace(/"/g,"")
+        color=color.replace("0x","#")
+        type=type.replace(/"/g,"")
+        Generator.addCode(`    ${type}(${txt})['style']="text-align:${align};color:${color}"`)
+    
+    }
+    
+    //% block="|- 添加正文[TXT]" blockType="command"
+    //% TXT.shadow="string" TXT.defl="正文文字"
+    export function html_add_p(parameter: any, block: any) {
+        let txt=parameter.TXT.code;
+        Generator.addCode(`    p(${txt})`)
+    
+    }
+    //% block="|- 添加加粗文字[TXT]" blockType="command"
+    //% TXT.shadow="string" TXT.defl="加粗文字"
+    export function html_add_b(parameter: any, block: any) {
+        let txt=parameter.TXT.code;
+        Generator.addCode(`    b(${txt})`)
+    
+    }
+
+    //% block="|- 添加[HB]" blockType="command"
+    //% HB.shadow="dropdown" HB.options="HB"
+    export function html_add_br(parameter: any, block: any) {
+        let hb=parameter.HB.code;
+        Generator.addCode(`    ${hb}()`)
+    
+    }
+
+    //% block="|- 添加图片 路径/static/[TXT] 宽度[WIDTH]" blockType="command"
+    //% TXT.shadow="normal" TXT.defl="img.png"
+    //% WIDTH.shadow="number" WIDTH.defl="600"
+    export function html_add_image(parameter: any, block: any) {
+        let txt=parameter.TXT.code;
+        let width=parameter.WIDTH.code;
+        Generator.addCode(`    img(src="./static/${txt}",width="${width}" )`)
+    
+    }
+    //% block="|- 添加链接[LINK] 文字[TXT] " blockType="command"
+    //% LINK.shadow="string" LINK.defl="https://unihiker.com"
+    //% TXT.shadow="string" TXT.defl="点击跳转"
+    export function html_add_a(parameter: any, block: any) {
+        let txt=parameter.TXT.code;
+        let link=parameter.LINK.code;
+        Generator.addCode(`    a(${txt},target="_blank",href=${link})`)
+    
+    }
+    //% block="|- 添加按钮 文字[TXT] 字号[SIZE] 执行函数为[ID]" blockType="command"
+    //% TXT.shadow="string" TXT.defl="A"
+    //% ID.shadow="normal" ID.defl="route_func"
+    //% SIZE.shadow="number" SIZE.defl="20"
+    export function html_add_input(parameter: any, block: any) {
+        let txt=parameter.TXT.code;
+        let id=parameter.ID.code;
+        let size=parameter.SIZE.code;
+        Generator.addCode(`    input_(id="${id}", type="button", value=${txt}, style="font-size:${size}px", onclick="button_${id}_Click()")`)
+
+        Generator.addDeclaration(`lsadd${id}`,`sc=sc+"function button_${id}_Click(){console.log('${id} click');let xhr = new XMLHttpRequest();xhr.open('post','/${id}');xhr.send('${id}');}"`)
+    
+        id=id.replace(/"/g,"")
+
+        Generator.addInit(`add_route_${id}`,`@flask_app.route('/${id}',methods=["POST"])
+def route_${id}():
+    return rec_${id}()`)
+    
+    
+    }
+
+
+    //% block="将网页对象[DOC]生成html代码并保存为文件[FILE].html" blockType="command"
+    //% DOC.shadow="normal" DOC.defl="doc"
+    //% FILE.shadow="normal" FILE.defl="index"
+    export function html_save_html(parameter: any, block: any) {
+        let file=parameter.FILE.code;
+        let doc=parameter.DOC.code;
+        Generator.addImport(`import os`)
+        Generator.addCode(`if not os.path.exists('./templates'):
+    os.makedirs('./templates')`)
+        Generator.addCode(`if not os.path.exists('./static'):
+    os.makedirs('./static')`)
+
+        Generator.addCode(`with open("./templates/${file}.html", "w", encoding="UTF8") as fileObj:
+    fileObj.write(str(${doc}))`)
+    
+    }
+
+}
